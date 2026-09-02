fdec9da408458cfd9d52c87b42648126da5102f9
leo.tian <leo.tian@aikendigital.com>
2026-09-02 23:37:06 +0800

    feat: 用户模块后端开发
    - 接入 MyBatis‑Flex 代码生成器，生成基础CRUD代码
    - 实现用户核心接口：注册、登录、获取登录用户信息、用户注销
    - 基于AOP实现全局权限校验逻辑
    - 完成管理员用户增删改查接口开发
    - 自定义序列化配置，解决Long类型返回前端丢失精度问题

---
+++ src/main/java/com/leo/ai/delivery/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.leo.ai.delivery.mapper;
+
+import com.leo.ai.delivery.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}

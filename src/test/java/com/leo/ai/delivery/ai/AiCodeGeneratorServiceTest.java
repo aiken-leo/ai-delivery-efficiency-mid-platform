f45fca212f86bb0233c8a66d97baad11c28594a9
leo.tian <leo.tian@aikendigital.com>
2026-09-04 18:45:01 +0800

    feat: 对话历史模块实现对话记忆持久化能力
    - 基于LangChain4j结合Redis实现对话记忆持久化存储
    - 通过工厂模式根据appId创建相互隔离的AI Service，引入Caffeine本地缓存做性能优化
    - 从数据库加载已有对话历史数据，完成完整对话记忆持久化逻辑

--- src/test/java/com/leo/ai/delivery/ai/AiCodeGeneratorServiceTest.java
+++ src/test/java/com/leo/ai/delivery/ai/AiCodeGeneratorServiceTest.java
@@ -24,4 +24,16 @@ class AiCodeGeneratorServiceTest {
         MultiFileCodeResult result = aiCodeGeneratorService.generateMultiFileCode("做个注册登录功能");
         Assertions.assertNotNull(result);
     }
+
+    @Test
+    void testChatMemory() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个程序员Leo的工具网站，总代码量不超过 20 行");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("不要生成网站，告诉我你刚刚做了什么？");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("做个程序员Leo的工具网站，总代码量不超过 20 行");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("不要生成网站，告诉我你刚刚做了什么？");
+        Assertions.assertNotNull(result);
+    }
 }

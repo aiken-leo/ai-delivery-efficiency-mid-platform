3730857fc74c4baebdc1b2b568d7c7f3b2e4c3cc
leo.tian <leo.tian@aikendigital.com>
2026-09-13 18:44:33 +0800

    refactor: 系统优化（稳定性优化） 1. 增加护轨逻辑自动重试机制，提升调用容错能力 2. 增加循环判定逻辑，防止AI工具调用进入无限循环，保障工作流稳定运行

--- src/main/java/com/leo/ai/delivery/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/leo/ai/delivery/ai/AiCodeGeneratorServiceFactory.java
@@ -122,7 +122,9 @@ public class AiCodeGeneratorServiceFactory {
                                 ToolExecutionResultMessage.from(toolExecutionRequest,
                                         "Error: there is no tool called " + toolExecutionRequest.name())
                         )
+                        .maxSequentialToolsInvocations(20)  // 最多连续调用 20 次工具
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+                        //.outputGuardrails(new RetryOutputGuardrail()) // 添加输出护轨，为了流式输出，这里不使用
                         .build();
             }
             // HTML 和 多文件生成，使用流式对话模型

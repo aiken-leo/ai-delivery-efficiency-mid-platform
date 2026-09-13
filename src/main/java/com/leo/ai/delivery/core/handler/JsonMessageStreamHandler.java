6af82666465fd65ed834a00a571f9be5ed84149d
leo.tian <leo.tian@aikendigital.com>
2026-09-13 18:34:29 +0800

    refactor: 系统优化（实时性优化） 1. 将Vue工程异步构建调整为同步构建，等待项目构建完成之后再结束SSE连接，保证前端可获取完整构建结果

--- src/main/java/com/leo/ai/delivery/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/leo/ai/delivery/core/handler/JsonMessageStreamHandler.java
@@ -6,8 +6,6 @@ import cn.hutool.json.JSONUtil;
 import com.leo.ai.delivery.ai.model.message.*;
 import com.leo.ai.delivery.ai.tools.BaseTool;
 import com.leo.ai.delivery.ai.tools.ToolManager;
-import com.leo.ai.delivery.constant.AppConstant;
-import com.leo.ai.delivery.core.builder.VueProjectBuilder;
 import com.leo.ai.delivery.model.entity.User;
 import com.leo.ai.delivery.model.enums.ChatHistoryMessageTypeEnum;
 import com.leo.ai.delivery.service.ChatHistoryService;
@@ -26,9 +24,6 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
-
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -60,9 +55,6 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息

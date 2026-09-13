b0d30d8e0956d2bbb5772b17d28ba8d537a9fdac
leo.tian <leo.tian@aikendigital.com>
2026-09-13 15:43:59 +0800

    refactor: 系统优化（并发优化 - Redis缓存性能优化） 1. 引入 Redis 缓存注解与配置类，统一缓存基础能力 2. 为精选应用列表接口添加 Redis 缓存，降低数据库访问压力，提升接口响应性能

--- src/main/java/com/leo/ai/delivery/controller/AppController.java
+++ src/main/java/com/leo/ai/delivery/controller/AppController.java
@@ -16,7 +16,6 @@ import com.leo.ai.delivery.exception.ThrowUtils;
 import com.leo.ai.delivery.model.dto.app.*;
 import com.leo.ai.delivery.model.entity.App;
 import com.leo.ai.delivery.model.entity.User;
-import com.leo.ai.delivery.model.enums.CodeGenTypeEnum;
 import com.leo.ai.delivery.model.vo.app.AppVO;
 import com.leo.ai.delivery.service.AppService;
 import com.leo.ai.delivery.service.ProjectDownloadService;
@@ -26,6 +25,7 @@ import com.mybatisflex.core.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
@@ -265,6 +265,11 @@ public class AppController {
      * @return 精选应用列表
      */
     @PostMapping("/good/list/page/vo")
+    @Cacheable(
+            value = "good_app_page",
+            key = "T(com.leo.ai.delivery.utils.CacheKeyUtils).generateKey(#appQueryRequest)",
+            condition = "#appQueryRequest.pageNum <= 10"
+    )
     public BaseResponse<Page<AppVO>> listGoodAppVOByPage(@RequestBody AppQueryRequest appQueryRequest) {
         ThrowUtils.throwIf(appQueryRequest == null, ErrorCode.PARAMS_ERROR);
         // 限制每页最多 20 个
